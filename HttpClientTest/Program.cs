9ff1a048b096f32793eeeda85cecbe56693b6252
Moses Nova <mosesabhisheknova@yahoo.com>
2018-02-07 13:08:54 +1300

    Initial Commit.

---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallWebAPIAsync()
+                    .Wait();
+        }
+        static async Task CallWebAPIAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:49576/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("api/values/5");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.Write(response.RequestMessage.ToString());
+                    //Department department = await response.Content.ReadAsAsync<Department>();
+                    //Console.WriteLine("Id:{0}\tName:{1}", department.DepartmentId, department.DepartmentName);
+                    //Console.WriteLine("No of Employee in Department: {0}", department.Employees.Count);
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
